00010cc3ea90e518eb75e0e83ea90460d05a3a0b
Qais Patankar <qaisjp@gmail.com>
2017-06-04 21:15:15 +0100

    It's question time!

--- src/index.js
+++ src/index.js
@@ -2,17 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Naming extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      locked: false,
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.setState({
+      locked: true,
+    });
+
+    this.props.onSubmit(e);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)} style={{display:"inline"}}>
+        <input value={this.props.username} disabled={this.state.locked} onChange={this.props.onChange} /> 
+      </form>
+    );
+  }
+}
+
 class HelloUser extends React.Component {
   constructor() {
     super();
     this.state = {
+      question: 1,
       username: '',
     };
   }
 
-  handleChange(e) {
+  handleChangeUsername(e) {
     this.setState({
       username: e.target.value,
+    });
+  }
+
+  progress() {
+    this.setState({
+      question: this.state.question + 1,
     });
   }
   
@@ -23,11 +56,36 @@ class HelloUser extends React.Component {
       you = <span>Hey, {this.state.username}!</span>;
     }
 
-    return <div>
-      Hello, I am {this.props.me}! <br />
-      What is your name? <input value={this.state.username} onChange={this.handleChange.bind(this)} /> <br />
-      <br /> {you}
-    </div>
+    const questions = [null,
+      (
+        <div>
+          Hello, I am {this.props.me}!
+          <br/>
+          What is your name? <Naming
+            username={this.state.username}
+            onChange={e => this.handleChangeUsername(e)}
+            onSubmit={e => this.progress()}
+          />
+          <br/><br/>
+          {you}
+        </div>
+      ),
+
+      (
+        <div>
+          {you}
+        </div>
+      )
+    ];
+
+    return (
+        <div>
+          You are on Question #{this.state.question}.
+          <hr/>
+
+          {questions[this.state.question]}
+        </div>
+    );
   }
 }
 
