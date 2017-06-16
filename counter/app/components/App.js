e484ef6b2deb0ebf2c346199f7f641fc64c60f65
Qais Patankar <qaisjp@gmail.com>
2017-06-16 13:29:57 +0100

    Initial counter buttons

--- counter/app/components/App.js
+++ counter/app/components/App.js
@@ -9,7 +9,9 @@ export default class App extends React.Component {
     render() {
         return (
             <div className='container'>
-                Hello, world!
+                <h1>{this.props.value}</h1>
+                <button onClick={this.props.onIncrement}>+</button>
+                <button onClick={this.props.onDecrement}>-</button>
             </div>
         );
     }
--- counter/app/index.js
+++ counter/app/index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
 import './index.css';
 
 import App from './components/App';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-)
+const counter = (state = 0, action) => {
+    switch (action.type) {
+    case 'INCREMENT':
+        return state + 1;
+    case 'DECREMENT':
+        return state - 1;
+    default:
+        return state;
+    }
+};
+
+const store = createStore(counter);
+const render = () => {
+    ReactDOM.render(
+        <App
+            value={store.getState()}
+            onIncrement={store.dispatch.bind(null, {type: "INCREMENT"})}
+            onDecrement={store.dispatch.bind(null, {type: "DECREMENT"})}
+        />,
+        document.getElementById('app')
+    );
+}
+
+store.subscribe(render);
+render();
