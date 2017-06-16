b364ec6bf5f3b109d2b9096929cfc33066bc1de6
Qais Patankar <qaisjp@gmail.com>
2017-06-16 12:58:59 +0100

    Counter boilerplate code

---
+++ counter/app/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                Hello, world!
+            </div>
+        );
+    }
+}
