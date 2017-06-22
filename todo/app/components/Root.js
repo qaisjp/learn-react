6993b476f1a49c672a26a7adc16002c1870c00a3
Qais Patankar <qaisjp@gmail.com>
2017-06-22 15:32:21 +0100

    initial use of routing in todo

--- todo/app/components/Root.js
+++ todo/app/components/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const Root = ({store}) => (
     <Provider store={store}>
-        <App/>
+        <Router>
+            <Route path='/' component={App} />
+        </Router>
     </Provider>
 );
 
