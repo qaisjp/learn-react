c2ddbef0b76ce0e13756c89fbde8d9a8dd2e1c80
Qais Patankar <qaisjp@gmail.com>
2017-06-23 04:08:21 +0100

    refactor todos reducer to own file

--- todo/app/reducers/index.js
+++ todo/app/reducers/index.js
@@ -1,38 +1,5 @@
 import { combineReducers } from 'redux';
-
-const toggleTodo = (todo, id) => {
-    if (todo.id !== id) {
-        return todo;
-    }
-
-    return {...todo, completed: !todo.completed};
-}
-
-const removeTodo = (state, id) => {
-    return state.filter(todo => todo.id !== id);
-}
-
-const todo = (state, action) => {
-    switch(action.type) {
-    case 'ADD_TODO':
-        return {id: action.id, text: action.text, completed: false};
-    case 'TOGGLE_TODO':
-        return toggleTodo(state, action.id);
-    }
-}
-
-const todos = (state = [], action) => {
-    switch (action.type) {
-    case 'ADD_TODO':
-        return [...state, todo(undefined, action)];
-    case 'REMOVE_TODO':
-        return removeTodo(state, action.id);
-    case 'TOGGLE_TODO':
-        return state.map(t => todo(t, action));
-    default:
-        return state;
-    }
-};
+import todos from './todos';
 
 const todoApp = combineReducers({todos});
 export default todoApp;
---
+++ todo/app/reducers/todos.js
@@ -0,0 +1,35 @@
+const toggleTodo = (todo, id) => {
+    if (todo.id !== id) {
+        return todo;
+    }
+
+    return {...todo, completed: !todo.completed};
+}
+
+const removeTodo = (state, id) => {
+    return state.filter(todo => todo.id !== id);
+}
+
+const todo = (state, action) => {
+    switch(action.type) {
+    case 'ADD_TODO':
+        return {id: action.id, text: action.text, completed: false};
+    case 'TOGGLE_TODO':
+        return toggleTodo(state, action.id);
+    }
+}
+
+const todos = (state = [], action) => {
+    switch (action.type) {
+    case 'ADD_TODO':
+        return [...state, todo(undefined, action)];
+    case 'REMOVE_TODO':
+        return removeTodo(state, action.id);
+    case 'TOGGLE_TODO':
+        return state.map(t => todo(t, action));
+    default:
+        return state;
+    }
+};
+
+export default todos;
